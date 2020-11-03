8cab7793181dd79b90a00d6157545e6c15fb480b
Proyecto_maro <u201817916@upc.edu.pe>
2020-11-03 16:42:28 -0500

    Initial commit

---
+++ src/main/java/com/acme/techfinance/TechfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.acme.techfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechfinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechfinanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/techfinance/TechfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.techfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechfinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
